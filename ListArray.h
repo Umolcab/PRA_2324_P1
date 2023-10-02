d65f15c2f699c0da59562acf047cde17d80b8a57
Unai Molla Caballero <umolcab@edu.upv.es>
2023-10-02 18:53:46 +0200

    Subida de archivos para duda en insert

--- ListArray.h
+++ ListArray.h
@@ -9,24 +9,24 @@ class ListArray : public List<T>{
 		int n; //numero de elementos
 		static const int MINSIZE = 2; //tamaño mínimo
 		void resize(int new_size){	//metodo para redimensionar
-			T* new_array = new ListArray<T>;
+			T* new_array = new T[new_size];
 			for(int i = 0; i < max; i++){
 				new_array[i] = arr[i];
 			}
-			std::destroy(arr);
+			delete arr;
 			arr = new_array;
 			max = new_size;
 		}
 
 	public:
 		ListArray(){
-			arr = new ListArray<T>;
-			max = MINSIZE;
+			arr = new T[MINSIZE];
+			max = 2;
 			n = 0;
 		}
 
 		~ListArray(){
-			std::destroy(arr);
+			delete arr;
 		}
 
 		T operator[](int pos){
@@ -39,17 +39,20 @@ class ListArray : public List<T>{
 
 		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
 			int i = 0;
-			out << "List [" << std::endl;
-			while(i <= list.n){
-				out << " " << list[i] << std::endl;
+			out << "List [";
+				while(i < list.n){
+				out << " " << list.arr[i] << std::endl;
 				i++;
 			}
-			out << "]" << std::endl;
+			out << "]";
 			return out;
 		}
 
 		void insert(int pos, T e) override{
 			if(pos >= 0 && pos < max){
+				for(int i=max; i>=pos; i--){
+					arr[i] = arr[i-1];
+				}
 				arr[pos] = e;
 				n++;
 			}else{
@@ -63,40 +66,40 @@ class ListArray : public List<T>{
 		}
 
 		void prepend(T e) override{
-			for(int i = n-1; i > 0; i--){
-				insert(arr[i+1],get(arr[i]));
-			}
 			insert(0, e);
 		}
 
 		T remove(int pos) override{
 			if(pos >= 0 && pos <= max-1){
-				arr[pos] = 0;
+				T aux = arr[pos];
+				for(int i = pos; i<max; i++){
+					arr[i] = arr[i+1];
+				}
 				resize(n-1);
 				n--;
-			}else{
-				throw std::out_of_range("La posición está fuera del array.");
+				return aux;
 			}
+			throw std::out_of_range("La posición está fuera del array.");
 		}
 
 		T get(int pos) override{
-			return arr[pos];
+			if(pos >= 0 && pos < n)
+				return arr[pos];
 		}
 
 		int search(T e) override{
 			int aux = -1;
 			for(int i = 0; i < n; i++){
-				if(e = arr[i]){
+				if(arr[i] == e){
 					aux = e;
 					break;
 				}
 			}
-
 			return aux;
 		}
 
 		bool empty() override{
-			return n = 0;
+			return n == 0;
 		}
 
 		int size() override{
