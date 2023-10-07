acf4dab8e0c722c6eb48b321dff3d727e58de7c9
Unai Molla Caballero <umolcab@edu.upv.es>
2023-10-07 13:23:51 +0200

    Resubida para preguntar una duda

--- ListLinked.h
+++ ListLinked.h
@@ -25,8 +25,9 @@ class ListLinked : public List<T> {
 				for(int i = 0; i < n; i++){
 					if(i = pos){
 						return aux->data;
+					}else{
+						aux = aux->next;
 					}
-					aux = aux->next;
 				}
 			}else{
 				throw std::out_of_range("Posición fuera del array.");
@@ -34,12 +35,12 @@ class ListLinked : public List<T> {
 		}
 
 		friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
-				out << "List [";
-				for(int i = 0; i < list.n; i++){
-					out << "\n" << list[i];
-				}
-				out << "]";
-				return out;
+			out << "List [";
+			for(int i = 0; i < list.n; i++){
+				out << "\n" << list.first[i];
+			}
+			out << "]";
+			return out;
 		}
 
 		void insert(int pos, T e){
@@ -50,20 +51,68 @@ class ListLinked : public List<T> {
 					preaux = aux;
 					aux = aux->next;
 				}
-				preaux->next = new Node<T>;
+				preaux->next = new Node(e);
 				preaux = preaux->next;
 				preaux->data = e;
 				preaux->next = aux;
+				n++;
 			}else{
 				throw std::out_of_range("Posición fuera del array.");
 			}
 		}
 
-		void append(T e);
-		void prepend(T e);
-		T remove(int pos);
-		T get(int pos);
-		int search(T e);
-		bool empty();
-		int size();
+		void append(T e){
+			insert(n, e);
+		}
+
+		void prepend(T e){
+			insert(0, e);
+		}
+
+		T remove(int pos){
+			Node<T>* aux = first;
+			Node<T>* preaux = nullptr;
+			if(pos >= 0 && pos <= n){
+				for(int i = 0; i < pos; i++){
+					preaux = aux;
+					aux = aux->next;
+				}
+				preaux->next = aux->next;
+				aux->next = nullptr;
+				n--;
+				return aux->data;
+			}else{
+				throw std::out_of_range("Posición fuera del array.");
+			}
+		}
+
+		T get(int pos){
+                    	if(pos >= 0 && pos <= n){
+				return first[pos];
+			}else{
+				throw std::out_of_range("Posición fuera del array.");
+			}
+		}
+
+		int search(T e){
+			Node<T>* aux = first;
+			int pos = -1;
+			T x;
+                        for(int i = 0; i < n; i++){
+				x = aux[i];
+				if(e == x){
+					pos = i;
+					break;
+				}
+			}
+			return pos;
+		}
+
+		bool empty(){
+			return n == 0;
+		}
+
+		int size(){
+			return n;
+		}
 };
