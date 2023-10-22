130316c6391c5f5f0c68a8842b96b6373c5434c5
Unai Molla Caballero <umolcab@edu.upv.es>
2023-10-22 23:39:23 +0200

    Añadida implementación de la clase Point2D y Makefile actualizado

---
+++ Point2D.cpp
@@ -0,0 +1,16 @@
+#include "Point2D.h"
+
+Point2D::Point2D(double x = 0, double y = 0) : x{x}, y{y} {}
+
+bool operator==(const Point2D &a, const Point2D &b){
+	return a.x == b.x && a.y ==b.x;
+}
+
+bool operator!=(const Point2D &a, const Point2D &b){
+	return a.x != b.x || a.y != b.y;
+}
+
+std::ostream& operator<<(std::ostream &out, const Point2D &p){
+	out << "El punto tiene las coordenadas: (" << p.x << "," << p.y << ")" << std::endl;
+	return out;
+}
