bb613e6994ea8dc46e2c91944cb9a49fee559c76
Unai Molla Caballero <umolcab@edu.upv.es>
2023-10-22 23:38:36 +0200

    Añadida declaración de la clase Point2D

--- Point2D.h
+++ Point2D.h
@@ -2,16 +2,19 @@
 #define POINT2D_H
 
 #include <ostream>
+#include <cmath>
 
 class Point2D{
 	public:
 		double x;
 		double y;
 		Point2D(double x, double y);
-		static double distance(const Point2D &a, const Point2D &b);
+		static double distance(const Point2D &a, const Point2D &b){
+			return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+		}
 		friend bool operator==(const Point2D &a, const Point2D &b);
 		friend bool operator!=(const Point2D &a, const Point2D &b);
-		friend std::ostream operator<<(std::ostream &out, const Point2D &p);
+		friend std::ostream& operator<<(std::ostream &out, const Point2D &p);
 };
 
 #endif
